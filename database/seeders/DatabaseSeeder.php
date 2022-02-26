dfc064e12aef2fd18b673a4c907c7b5eeef0d9d2
justinmanigo <justigraig.manigo15@gmail.com>
2022-02-26 22:53:47 +0800

    added user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('users')->insert([
+            [
+                'email' => 'admin@email.com',
+                'password' => Hash::make('test'),
+                'name' => 'super',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            
+           
+           
+        ]);
     }
 }
